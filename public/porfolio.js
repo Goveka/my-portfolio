832ae5c28e77e73d83b98ee4bc551be4ad9c1c8a
unknown <sizwenkala80@gmail.com>
2023-01-21 05:12:38 +0200

    make it better

--- public/porfolio.js
+++ public/porfolio.js
@@ -1,2 +1,43 @@
-let nam= "sizwe"
-console.log(nam)
+const id= "63cac530ec7d9e5bab64828c";
+const request= new XMLHttpRequest();
+request.open('Post', '/viewedSite');
+request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+request.send(`id=${id}`)
+
+
+const sourceCode = document.querySelectorAll('.source-code').forEach(function(codeBtn){
+    codeBtn.addEventListener('click',()=>{
+        const id= "63cac530ec7d9e5bab64828c";
+const request= new XMLHttpRequest();
+request.open('Post', '/viewedProjectCode');
+request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+request.send(`id=${id}`)
+    })
+})
+
+const viewProject = document.querySelectorAll('.viewProject').forEach(function(codeBtn){
+    codeBtn.addEventListener('click',()=>{
+        const id= "63cac530ec7d9e5bab64828c";
+const request= new XMLHttpRequest();
+request.open('Post', '/viewedProject');
+request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+request.send(`id=${id}`)
+    })
+})
+
+
+const githubProfile= document.getElementById('github-profile').addEventListener('click',()=>{
+    const id= "63cac530ec7d9e5bab64828c";
+    const request= new XMLHttpRequest();
+    request.open('Post', '/viewedGITHUB');
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.send(`id=${id}`)
+})
+
+const downloadedCV= document.getElementById('download-cv').addEventListener('click', ()=>{
+    const id= "63cac530ec7d9e5bab64828c";
+    const request= new XMLHttpRequest();
+    request.open('Post', '/downloadedCV');
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.send(`id=${id}`) 
+})
--- server.js
+++ server.js
@@ -1,12 +1,155 @@
 const express = require('express');
 const app = express();
-const port=process.env.PORT || 4009
+const port=process.env.PORT || 4009;
+//const url= "mongodb://localhost:27017/test";
+const bodyParser= require('body-parser');
+const mongoose= require('mongoose');
+const cors= require('cors');
+const url='mongodb+srv://Sizwenkala:sizwe123@cluster0.fejtt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 
-app.use(express.static('public'))
+// middleware
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors())
+
+
+// connecting to database
+mongoose.connect(url,{useNewUrlParser: true,useUnifiedTopology: true})
+
+const database = mongoose.connection
+//checking if our connection to database was successful
+database.on('error', (error)=>{
+    console.log(error)
+})
+database.once('connected', ()=>{
+    console.log("database connected")
+})
+
+
+
+// creating a new monngoose model
+const View = mongoose.model('View', new mongoose.Schema({
+  viewedSite: Number,
+  viewedProject: Number,
+  viewedProjectCode: Number,
+  viewedGITHUB: Number,
+  downloadedCV: Number,
+  date: String
+}));
+
+
+
 
 app.get('/',(req,res)=>{
   res.sendFile(__dirname + '/portfolio.html')
 })
+
+
+// posting to the database || updating the object in the database
+app.post('/viewedSite', (res,req)=>{
+  const id="63cb52231c868773a7297ef6"
+  View.findOne({_id: id}, (err, object)=>{
+      if (err) {
+          res.sendStatus(500)
+      }else {
+          object.viewedSite += 1;
+          object.date= new Date()
+          object.save(function(err, res){
+              if (err){
+                  res.sendStatus(500)
+                  console.error(err)
+              }else {
+                 // res.sendStatus(200)
+                  console.log('updated viewSite');
+              }
+          })
+      }
+  })
+})
+
+app.post('/viewedProjectCode', (res,req)=>{
+  const id="63cb52231c868773a7297ef6"
+  View.findOne({_id: id}, (err, object)=>{
+      if (err) {
+          res.sendStatus(500)
+      }else {
+          object.viewedProjectCode += 1;
+          object.save(function(err, res){
+              if (err){
+                  res.sendStatus(500)
+                  console.error(err)
+              }else {
+                 // res.sendStatus(200)
+                  console.log('updated viewedProject code');
+              }
+          })
+      }
+  })
+})
+
+app.post('/viewedProject', (res,req)=>{
+  const id="663cb52231c868773a7297ef6"
+  View.findOne({_id: id}, (err, object)=>{
+      if (err) {
+          res.sendStatus(500)
+      }else {
+          object.viewedProject += 1;
+          object.save(function(err, res){
+              if (err){
+                  res.sendStatus(500)
+                  console.error(err)
+              }else {
+                 // res.sendStatus(200)
+                  console.log('updated viewedProject');
+              }
+          })
+      }
+  })
+})
+
+app.post('/viewedGITHUB', (res,req)=>{
+  const id="63cb52231c868773a7297ef6"
+  View.findOne({_id: id}, (err, object)=>{
+      if (err) {
+          res.sendStatus(500)
+      }else {
+          object.viewedGITHUB += 1;
+          object.save(function(err, res){
+              if (err){
+                  res.sendStatus(500)
+                  console.error(err)
+              }else {
+                 // res.sendStatus(200)
+                  console.log('updated viewed github profile');
+              }
+          })
+      }
+  })
+})
+
+app.post('/downloadedCV', (res,req)=>{
+  const id="63cb52231c868773a7297ef6"
+  View.findOne({_id: id}, (err, object)=>{
+      if (err) {
+          res.sendStatus(500)
+      }else {
+          object.downloadedCV += 1;
+          object.save(function(err, res){
+              if (err){
+                  res.sendStatus(500)
+                  console.error(err)
+              }else {
+                 // res.sendStatus(200)
+                 console.log('updated || downloaded cv');
+              }
+          })
+      }                  
+  })
+})
+
 
 app.listen(port, console.log('server is up on port'+':' + port))
